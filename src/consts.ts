fafafdfea854a5e666a8e8909c6e6ba6aeef47b8
James Littlewood <james@Jamess-MacBook-Pro.local>
2026-06-17 20:01:02 +0100

    Fix website links

--- src/consts.ts
+++ src/consts.ts
@@ -1,31 +1,14 @@
 // Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'James Littlewood'
-export const SITE_DESCRIPTION =
-	'James Littlewood Personal Portfolio Website'
+export const SITE_DESCRIPTION = 'James Littlewood Personal Portfolio Website'
 
-// If you deploy to a subdirectory, set the `WEBSITE_BASE` here.
-// e.g. '/blog' if you are deploying to example.com/blog
-// Leave it as an empty string if you are deploying to a root domain (e.g. example.com)
-export const SITE_BASE = '/saral-theme-astro'
+export const SITE_BASE = ''
 
 export const SocialLinks = [
 	{
 		name: 'GitHub',
-		url: '',
-	},
-	{
-		name: 'LinkedIn',
-		url: '',
-	},
-	{
-		name: 'X / Twitter',
-		url: '',
-	},
-	{
-		name: 'Instagram',
-		url: '',
+		url: 'https://github.com/jameslittlewood',
 	},
 ]
 
@@ -33,10 +16,6 @@ export const WebsiteLinks = [
 	{
 		name: 'Home',
 		url: 'https://jameslittlewood.net',
-	},
-	{
-		name: 'Blog',
-		url: 'blog',
 	},
 	{
 		name: 'About',
